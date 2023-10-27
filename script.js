2f9836ef14ad73981934df7eb38183bdf3f12f0b
Tsion <tsionwendmu12@gmail.com>
2023-10-27 16:44:24 -0400

    added a render to the scene

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 //scene
 const  scene = new THREE.Scene()
 
@@ -15,4 +16,15 @@ const sizes = {
 
 //camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width /sizes.height)
+camera.position.z = 3
+camera.position.x = 2
 scene.add(camera)
+
+//Renderer
+const canvas = document.querySelector('.webgl')
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+
+renderer.render(scene, camera)
